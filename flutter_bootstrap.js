26c6fb8981fe81276fc1f182c149099d4f0660f1
Biraj Giri <birajgiri21@gmail.com>
2025-11-10 22:26:47 -0500

    Deploy updated Pokemon TCG app with battle feature

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2010921829"
+    serviceWorkerVersion: "3604918893"
   }
 });
